26d194e2fd859f871f0636301d62b39e22b84957
rose-tetteh <rose.tetteh@amalitech.com>
2025-03-26 13:20:15 +0000

    Update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM maven:3.9.6-eclipse-temurin-21 AS build
 WORKDIR /app
-
 COPY pom.xml .
+RUN mvn dependency:go-offline -B
 COPY src ./src
-ARG SECRETS_MANAGER_SECRET_ARN
+RUN mvn package -DskipTests
+
+FROM eclipse-temurin:21-jre
+WORKDIR /app
+COPY --from=build /app/target/*.jar app.jar
 ENV SECRETS_MANAGER_SECRET_ARN=${SECRETS_MANAGER_SECRET_ARN}
-COPY target/*.jar app.jar
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/example/awsdbapp/service/ImageService.java
+++ src/main/java/com/example/awsdbapp/service/ImageService.java
@@ -14,7 +14,6 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
